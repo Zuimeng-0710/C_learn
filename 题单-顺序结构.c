8ec096a3c4c4933abb83f8b931c14b80d5e36a14
zuim <3256553537@qq.com>
2025-10-12 22:18:00 +0800

    sequentialStructure

--- 题单-顺序结构.c
+++ 题单-顺序结构.c
@@ -121,3 +121,72 @@ int main() {
 	printf("%.1lf", S);
 	return 0;
 }
+
+//再分肥宅水
+int  main() {
+	double t;
+	int n;
+	scanf("%lf %d", &t, &n);
+	printf("%.3lf\n", t / (2 * n) * 2);
+	printf("%d", 2 * n);
+	return 0;
+}
+
+//A+B problem
+int main(){
+    int  num1,num2;
+    scanf("%d %d",&num1,&num2);
+    printf("%d",num1 + num2);
+    return 0;
+}
+
+//上学迟到
+#include <cstdio>
+int main()
+{
+	int S,V;//家到学校的距离 //行走的速度 
+	scanf("%d%d",&S,&V);
+	int t;
+	if(S%V==0)//因为后面是从07：59开始的,所以这里的时间少一分钟 
+	{
+		t=S/V;
+		t+=9;//垃圾分类的时间 
+	}
+	else
+	{
+		t=S/V;//时间如果不能被速度整除,就要提前一分钟出发 
+		t+=10;//垃圾分类的时间 
+	}
+	int H,M;//保存出发的小时,分钟 
+	H=7;
+	M=59;
+	while(t!=0) 
+	{
+		if(M!=0)
+		{
+			M--;
+		}
+		else
+		{
+			if(H!=0)
+			{
+				H--;
+				M=59;
+			}
+			else
+			{
+				H=23;
+				M=59;//时间重置
+			}
+		}
+		t--;
+	}
+	int h1,h2;//分开保存小时，分钟的十位，个位 
+	int m1,m2;
+	h1=H/10;
+	h2=H-(10*(H/10)); 
+	m1=M/10;
+	m2=M-(10*(M/10)); 
+	printf("%d%d:%d%d",h1,h2,m1,m2);
+}
+
